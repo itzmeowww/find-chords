d4826484c40bc4840f10505be1f787ac6574bb32
itzmeowww <winthanasan@gmail.com>
2021-06-17 10:59:01 +0700

    jsut add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,112 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Text, Flex, Box, Tooltip, Divider } from "@chakra-ui/react";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Container } from "../components/Container";
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
-      </Text>
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
+import song from "../data/song.json";
+interface sectionInterface {
+  info: string;
+  noLyric?: boolean;
+}
+const Index = () => {
+  const createSection = (ly: sectionInterface) => {
+    let ret = [];
+    let text = "";
+    let chord = "";
+    const noLyric = ly.noLyric === undefined ? false : ly.noLyric;
+    for (let idx = 0; idx < ly.info.length; idx++) {
+      if (ly.info[idx] == "[") {
+        idx++;
+        while (ly.info[idx] != "]") {
+          chord += ly.info[idx];
+          idx++;
+        }
+      } else if (ly.info[idx] == " " || idx == ly.info.length - 1) {
+        if (idx == ly.info.length - 1) text += ly.info[idx];
+
+        ret.push(
+          <Flex
+            h="4em"
+            mr="5px"
+            justify="flex-end"
+            flexDir="column"
+            overflow="visible"
           >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+            <Tooltip
+              label={chord}
+              isOpen={true}
+              placement="top-start"
+              fontSize="xs"
+              hasArrow={!noLyric}
+              modifiers={[
+                {
+                  name: "flip",
+                  options: {
+                    fallbackPlacements: [],
+                  },
+                },
+                {
+                  name: "preventOverflow",
+                  options: {
+                    mainAxis: false,
+                    altAxis: false,
+                  },
+                },
+              ]}
+            >
+              <Text whiteSpace="nowrap" fontFamily="sanSerif" fontWeight="thin">
+                {text}&nbsp;
+              </Text>
+            </Tooltip>
+          </Flex>
+        );
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+        chord = "";
+        text = "";
+      } else {
+        text += ly.info[idx];
+      }
+    }
+    return ret;
+  };
 
-export default Index
+  const ret = song.chord.map((x) => {
+    return createSection(x);
+  });
+
+  return (
+    <Container minHeight="100vh">
+      <Box h="20vh"></Box>
+      <Text fontSize="4xl" fontWeight="bold" colorScheme="blackAlpha">
+        {song.title}
+      </Text>
+      <Text fontSize="xl" colorScheme="gray">
+        {song.artist}
+      </Text>
+      <Box h="10vh"></Box>
+      <Flex flexDir="column" justify="center">
+        {ret.map((x, id) => {
+          return (
+            <Flex
+              maxW={["80vw", "30vw"]}
+              flexDir="row"
+              flexWrap="wrap"
+              justify="center"
+              key={id}
+            >
+              {x.map((xx) => {
+                return xx;
+              })}
+              <Divider mt="20px" mb="10px" w="80%" variant="dashed" />
+            </Flex>
+          );
+        })}
+      </Flex>
+      <Box h="50vh" w="100%"></Box>
+
+      <DarkModeSwitch />
+    </Container>
+  );
+};
+export default Index;
